a434e7a1f01c38d2042e042cad35a850f77d926f
yyoshiokasfdc <yoshiokay1998@gmail.com>
2021-12-14 09:13:13 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @pagy, @microposts = pagy(current_user.microposts.order(id: :desc))
+      @pagy, @microposts = pagy(current_user.feed_microposts.order(id: :desc))
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
